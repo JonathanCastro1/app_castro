6012e5dc6d9cb6415446174b5e5de459cfa7ce77
jonathan <jonathancastro2018@yahoo.com>
2018-12-06 23:04:33 -0400

    agregando componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { LoginService } from './services/login.service';
+
+import {FormsModule} from '@angular/forms';
+
+/* mport {HttpModule} from '@angular/http'; */
+
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
+/* para las rutas */
+const appRoutes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'registro', component:RegistroComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'usuarios', component:UsuariosComponent},
+  {path:'login', component:LoginComponent}
+ 
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    HeaderComponent,
+    DashboardComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  login: 'https://localhost:8080/login',
+  allUsers: 'https://localhost:8080/usuarios'
 };
 
 /*
